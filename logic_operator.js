0de384510d5ce596aa5f34c20c88400cf7ed7757
Demon154 <chriswhitelol77@gmail.com>
2022-08-26 21:28:39 +0500

    add logic_operator.js

---
+++ logic_operator.js
@@ -0,0 +1,6 @@
+// logical operators //
+// there are four logical operators in Javascript these are
+//  the (OR) operator which is represented by ||
+// the (AND) operator which is represented by &&
+// the (NOT) operator which is represented by !
+// and the (nullish coalescing) which is represented by ??
